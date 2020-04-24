948572ed9c0dafb569a8e42b7dd45aadd9c12d26
florianthalmann <florianthalmann@bluewin.ch>
2020-04-24 17:07:56 +0900

    added geoloc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import * as _ from 'lodash';
 import { Component } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { OnboshiPlayer } from '../services/player.service';
 
 @Component({
@@ -12,13 +13,27 @@ export class HomePage {
   protected x = 0;
   protected y = 0;
   
-  constructor(private player: OnboshiPlayer) {
-    this.automove();
+  constructor(private player: OnboshiPlayer, private geolocation: Geolocation) {
+    //this.automove();
+    this.geomove();
   }
   
   protected updatePosition() {
     console.log("pos", this.x, this.y);
     this.player.setPosition(this.x/1000, this.y/1000);
+  }
+  
+  private geomove() {
+    //35.03 35.07, 135.77 135.8
+    const minLat = 35.03, maxLat = 35.07;
+    const minLong = 135.77, maxLong = 135.8;
+    this.geolocation.watchPosition().subscribe((data) => {
+     console.log(data.coords.latitude, data.coords.longitude)
+     this.x = (data.coords.longitude-minLong)/(maxLong-minLong)*1000;
+     this.y = (data.coords.latitude-minLat)/(maxLat-minLat)*1000;
+     console.log(this.x, this.y)
+     this.updatePosition();
+    });
   }
   
   private automove() {
